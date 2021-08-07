98a1943e4b99196ada2236f45c254b9494ce9569
yamadamael <yamadamael@gmail.com>
2021-08-08 02:24:57 +0900

    行列計算修正

--- Matrix4x3.cpp
+++ Matrix4x3.cpp
@@ -458,9 +458,9 @@ void Matrix4x3::setupReflect(const Vector3 &n)
 Vector3 operator*(const Vector3 &p, const Matrix4x3 &m)
 {
     return Vector3(
-        p.x * m.m11 + p.y * m.m21 + p.z * m.m31,
-        p.x * m.m12 + p.y * m.m22 + p.z * m.m32,
-        p.x * m.m13 + p.y * m.m23 + p.z * m.m33);
+        p.x * m.m11 + p.y * m.m21 + p.z * m.m31 + m.tx,
+        p.x * m.m12 + p.y * m.m22 + p.z * m.m32 + m.ty,
+        p.x * m.m13 + p.y * m.m23 + p.z * m.m33 + m.tz);
 }
 
 Vector3 &operator*=(Vector3 &p, const Matrix4x3 &m)
@@ -474,17 +474,17 @@ Matrix4x3 operator*(const Matrix4x3 &a, const Matrix4x3 &b)
 {
     Matrix4x3 r;
 
-    r.m11 = a.m11 * b.m11 * a.m12 * b.m21 * a.m13 * b.m31;
-    r.m12 = a.m11 * b.m12 * a.m12 * b.m22 * a.m13 * b.m32;
-    r.m13 = a.m11 * b.m13 * a.m12 * b.m23 * a.m13 * b.m33;
+    r.m11 = a.m11 * b.m11 + a.m12 * b.m21 + a.m13 * b.m31;
+    r.m12 = a.m11 * b.m12 + a.m12 * b.m22 + a.m13 * b.m32;
+    r.m13 = a.m11 * b.m13 + a.m12 * b.m23 + a.m13 * b.m33;
 
-    r.m21 = a.m21 * b.m11 * a.m22 * b.m21 * a.m23 * b.m31;
-    r.m22 = a.m21 * b.m12 * a.m22 * b.m22 * a.m23 * b.m32;
-    r.m22 = a.m21 * b.m13 * a.m22 * b.m23 * a.m23 * b.m33;
+    r.m21 = a.m21 * b.m11 + a.m22 * b.m21 + a.m23 * b.m31;
+    r.m22 = a.m21 * b.m12 + a.m22 * b.m22 + a.m23 * b.m32;
+    r.m23 = a.m21 * b.m13 + a.m22 * b.m23 + a.m23 * b.m33;
 
-    r.m31 = a.m31 * b.m11 * a.m32 * b.m21 * a.m33 * b.m31;
-    r.m32 = a.m31 * b.m12 * a.m32 * b.m22 * a.m33 * b.m32;
-    r.m33 = a.m31 * b.m13 * a.m32 * b.m23 * a.m33 * b.m33;
+    r.m31 = a.m31 * b.m11 + a.m32 * b.m21 + a.m33 * b.m31;
+    r.m32 = a.m31 * b.m12 + a.m32 * b.m22 + a.m33 * b.m32;
+    r.m33 = a.m31 * b.m13 + a.m32 * b.m23 + a.m33 * b.m33;
 
     r.tx = a.tx * b.m11 + a.ty * b.m21 + a.tz * b.m31 + b.tx;
     r.ty = a.ty * b.m12 + a.ty * b.m22 + a.tz * b.m32 + b.ty;
